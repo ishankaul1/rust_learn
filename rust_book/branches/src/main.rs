c91d105bb033c910e21fd8bfb0fa4d858a73ea4e
Ishan Kaul <ishankaul2000@gmail.com>
2025-07-07 21:57:44 -0700

    finish up some branches logic

--- rust_book/branches/src/main.rs
+++ rust_book/branches/src/main.rs
@@ -46,18 +46,45 @@
 //     }
 // }
 
+// Returning from loops
+// fn main() {
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("final result: {result}");
+// }
+
+// // while
+// fn main() {
+//     let mut number = 3;
+//     while number != 0 {
+//         println!("{number}");
+//         number -= 1;
+//     }
+
+//     println!("LIFTOFF!!!!");
+// }
+
+// for loop
+
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     for element in a {
+//         println!("The value is: {element}")
+//     }
+// }
+
+// range + rev
+
 fn main() {
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("final result: {result}");
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+    println!("LIFTOFF!");
 }
-
-
-// UP NEXT: Loop labels & nested loops
